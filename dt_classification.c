aa9c7b77bd4a88c94d726647e39902ea28e8a7ff
MatMyfta <mateomyftaraj98@gmail.com>
2022-02-26 15:25:54 +0100

    debug print and dt warning fix

--- dt_classification.c
+++ dt_classification.c
@@ -4,7 +4,7 @@ uint16_t decision_tree_classifier(struct Node* root, fixed X[])
 {
     if(X[root->feature] < root->threshold) {
 		if(root->left != NULL) {
-			decision_tree_classifier(root->left, X);
+			return decision_tree_classifier(root->left, X);
 		}
 		else {
 			return root->left_class;
@@ -12,7 +12,7 @@ uint16_t decision_tree_classifier(struct Node* root, fixed X[])
 	}
     else {
 		if(root->right != NULL) {
-			decision_tree_classifier(root->right, X);
+			return decision_tree_classifier(root->right, X);
 		}
 		else {
 			return root->right_class;
